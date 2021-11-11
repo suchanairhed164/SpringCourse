662a500e2ea648b4a9da88dd575774616280ae43
Victor <victor98@bk.ru>
2021-11-11 09:43:50 +0800

    Initial commit

---
+++ src/main/java/com/example/springcourse/Application.java
@@ -0,0 +1,13 @@
+package com.example.springcourse;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+
+public class Application {
+}
